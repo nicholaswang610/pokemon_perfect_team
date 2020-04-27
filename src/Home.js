1ccc23993d00bba04d5321c2b0da129cd1a27201
Nicholas Wang <wangnich@gmail.com>
2020-04-26 19:10:20 -0700

    fixing issue where component kept remounting

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import axios from 'axios';
+import Pokemon from './Pokemon.js';
 
 class Home extends Component
 {
     componentDidMount(){
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+        console.log('MOUNTED');
+        if(this.props.pokemon.length !== 151){
+            axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
             .then(res => {
                 const data = res.data.results;
-                const pokemon = data.map(mon=>{
-                    axios.get(mon.url).then(res=>{this.props.initializePokemon(res)});
+                const pokemon = data.map((mon, index)=>{
+                    axios.get(mon.url).then(res=>{this.props.initializePokemon(res, index)});
                 });
+                return pokemon;
             });
+        }
     }
+
     render(){
-        console.log(this.props);
-        const pokemonList = this.props.pokemon.length === 151 ? (this.props.pokemon.map(mon => {
-            return(
-                <div>
-                    <img src={mon.data.sprites.front_default}></img>
-                </div>
-            )
-        })):(<p>Loading pok&eacute;mon...</p>)
+        console.log(this.props.pokemon);
         return <div>
-            {pokemonList}
+            <Pokemon pokemon={this.props.pokemon}/>
         </div>
     }
 }
@@ -33,12 +32,12 @@ const mapStateToProps = (state, ownProps) => {
         {
             pokemon: state.pokemon
         }
-    )
+    );
 }
 const mapDispatchToProps = (dispatch) => {
     return(
         {
-            initializePokemon: pokemon => {dispatch({type:"INITIALIZE", pokemon:pokemon})}
+            initializePokemon: (pokemon,index) => {dispatch({type:"INITIALIZE", pokemon:pokemon, id:index})},
         }
     );
 }
