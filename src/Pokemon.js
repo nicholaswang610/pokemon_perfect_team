4df35fc80b481c08c9c98fd5189df147be5c904e
Nicholas Wang <wangnich@gmail.com>
2020-05-09 18:49:20 -0700

    ui updates

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,49 +1,41 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-class Pokemon extends Component{
-
-
-
-    toggleChosen = e => {
-        if(e.target.parentElement.classList.contains('portrait') && !e.target.parentElement.classList.contains('chosen') && this.props.chosen.length < 6){
-            const filteredMon = this.props.pokemon.filter(mon=>{
-                return e.target.alt === mon.data.name;
-            })[0];
-            this.props.addChosen(filteredMon);
-        }
-        else if(e.target.classList.contains('portrait') && !e.target.classList.contains('chosen') && this.props.chosen.length < 6){
-            const filteredMon = this.props.pokemon.filter(mon=>{
-                return e.target.childNodes[0].alt === mon.data.name;
-            })[0];
-            this.props.addChosen(filteredMon);
-        }
-        else if(e.target.parentElement.classList.contains('portrait') && e.target.parentElement.classList.contains('chosen')){
-            const filteredMon = this.props.pokemon.filter(mon=>{
-                return e.target.alt === mon.data.name;
-            })[0];
-            this.props.removeChosen(filteredMon);
-        }
-        else if(e.target.classList.contains('portrait') && e.target.classList.contains('chosen')){
-            const filteredMon = this.props.pokemon.filter(mon=>{
-                return e.target.childNodes[0].alt === mon.data.name;
-            })[0];
-            this.props.removeChosen(filteredMon);
-        }
-    }
+class Team extends Component
+{
     render(){
-        const pokemonList = this.props.pokemon.length === 151 ? (this.props.pokemon.map(mon => {
-            return(
-                 <span className={this.props.chosen.includes(mon) ? 'portrait chosen' : 'portrait'} key={mon.id} onClick={e=>{this.toggleChosen(e)}}>
-                    <img alt={mon.data.name} src={mon.data.sprites.front_default}></img>
-                </span>
-            )
-            })):(<p>Loading pok&eacute;mon...</p>);
-        return(
-            <div>
-                {pokemonList}
+        console.log(this.props.chosen);
+        const chosenList = this.props.chosen.length > 0 ? (this.props.chosen.map(pokemon=>{
+            return (<div key={pokemon.data.id} className="member">
+                    <div className="px-2 text-left">
+                        <img alt={pokemon.data.name} src={pokemon.data.sprites.front_default}/>
+                        <p className='name'>{pokemon.data.name}</p>
+                    </div>
+                </div>);
+        })):(<div>No teammates chosen yet!</div>);
+        if(chosenList.length > 0)
+        {
+            return <div>
+                <div className="table table-borderless">
+                    <div className="row my-4 py-4 px-4 mx-4">
+                        <td className="w-50 text-left">{chosenList[0]}</td>
+                        <td className="w-50 text-left">{chosenList[1]}</td>
+                    </div>
+                    <div className="row justify-content-center my-4 py-4 px-4 mx-4">
+                        <td className="w-50 text-left">{chosenList[2]}</td>
+                        <td className="w-50 text-left">{chosenList[3]}</td>
+                    </div>
+                    <div className="row justify-content-center my-4 py-4 px-4 mx-4">
+                        <td className="w-50 text-left">{chosenList[4]}</td>
+                        <td className="w-50 text-left">{chosenList[5]}</td>
+                    </div>
+                </div>
             </div>
-         );
+        }
+        else{
+            return <div className="text-center container">{chosenList}</div>
+        }
+        
     }
 }
 
@@ -58,11 +50,10 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
     return(
         {
-            addChosen: pokemon=>{dispatch({type:"ADD_CHOSEN", pokemon:pokemon})},
-            removeChosen:pokemon=>{dispatch({type:"REMOVE_CHOSEN", pokemon:pokemon})}
         }
     );
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(Pokemon);
+export default connect(mapStateToProps,mapDispatchToProps)(Team);
+
