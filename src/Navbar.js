37f6d3c40fc9e3912f8c2d7b71f9d372fa75776b
Nicholas Wang <wangnich@gmail.com>
2020-05-12 18:25:33 -0700

    more ui updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,22 +4,22 @@ import {connect} from 'react-redux';
 
 const Navbar = (props) => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container my-2">
                 <img className="logo" alt="PokeTeam Builder" src={require("./images/PokeTeam-Builder.png")}></img>
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <NavLink className="nav-link" exact to="/">Home</NavLink>
+                        <NavLink className="nav-link text-light" exact to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
                         <div className="cart">
-                            <NavLink className="nav-link" to="/my-team">My Team <span className="track-cart-items">{props.chosen.length}</span></NavLink>
+                            <NavLink className="nav-link text-light" to="/my-team">My Team <span className="track-cart-items">{props.chosen.length}</span></NavLink>
                         </div>
                     </li>
                 </ul>  
-            </div>    
+            </div>
         </nav>
-        
+
     );
 }
 
