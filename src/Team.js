c557b802585464e00da9939855fbbb5eadcaa944
Nicholas Wang <wangnich@gmail.com>
2020-05-12 18:26:45 -0700

    more ui updates

--- src/Team.js
+++ src/Team.js
@@ -1,39 +1,60 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
 class Team extends Component
 {
     render(){
         console.log(this.props.chosen);
         const chosenList = this.props.chosen.length > 0 ? (this.props.chosen.map(pokemon=>{
-            return (<div key={pokemon.data.id} className="member">
-                    <img alt={pokemon.data.name} src={pokemon.data.sprites.front_default}/>
-                    <div className="stats">
-                        <p>{pokemon.data.name}</p>
+            console.log(pokemon.data.types[0].type.name);
+            if(pokemon.data.types.length === 2)
+            {
+                return (
+                <div key={pokemon.data.id} className="member">
+                    <span className={"multiple-types " + pokemon.data.types[0].type.name}></span>
+                    <span className={"multiple-types " + pokemon.data.types[1].type.name}></span>
+                    <div className="details px-2 py-3 text-left">
+                        <img alt={pokemon.data.name} src={pokemon.data.sprites.front_default}/>
+                        <p className='name text-light'>{pokemon.data.name}</p>
                     </div>
                 </div>);
-        })):(<div>No teammates chosen yet!</div>);
+            }else if(pokemon.data.types.length === 1)
+            {
+                return(
+                    <div key={pokemon.data.id} className="member">
+                        <span className={"type " + pokemon.data.types[0].type.name}></span>
+                        <div className="details px-2 py-3 text-left">
+                            <img alt={pokemon.data.name} src={pokemon.data.sprites.front_default}/>
+                            <p className='name text-light'>{pokemon.data.name}</p>
+                        </div>
+                    </div>);
+            }else{
+                return <div>There was an error!</div>;
+            }
+            
+        })):(<div className="text-light">No teammates chosen yet!</div>);
         if(chosenList.length > 0)
         {
-            return <div className="member-container">
-                <div className="table table-borderless">
-                    <div className="row justify-content-center my-4 py-4 px-4 mx-4">
-                        <td>{chosenList[0]}</td>
-                        <td>{chosenList[1]}</td>
-                    </div>
-                    <div className="row justify-content-center my-4 py-4 px-4 mx-4">
-                        <td>{chosenList[2]}</td>
-                        <td>{chosenList[3]}</td>
-                    </div>
-                    <div className="row justify-content-center my-4 py-4 px-4 mx-4">
-                        <td>{chosenList[4]}</td>
-                        <td>{chosenList[5]}</td>
-                    </div>
-                </div>
+            return <div>
+                <table className="table table-borderless">
+                    <tbody>
+                        <tr className="row my-4 py-4 px-4 mx-4">
+                            <td className="w-50 text-left">{chosenList[0]}</td>
+                            <td className="w-50 text-left">{chosenList[1]}</td>
+                        </tr>
+                        <tr className="row justify-content-center my-4 py-4 px-4 mx-4">
+                            <td className="w-50 text-left">{chosenList[2]}</td>
+                            <td className="w-50 text-left">{chosenList[3]}</td>
+                        </tr>
+                        <tr className="row justify-content-center my-4 py-4 px-4 mx-4">
+                            <td className="w-50 text-left">{chosenList[4]}</td>
+                            <td className="w-50 text-left">{chosenList[5]}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         }
         else{
-            return <div className="text-center member-container">{chosenList}</div>
+            return <div className="text-center container">{chosenList}</div>
         }
         
     }
